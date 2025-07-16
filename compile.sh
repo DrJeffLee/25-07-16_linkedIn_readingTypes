9242a9405193cabf7dada23a249a0211281baaa9
Dr Jeffrey Lee Johnston <jeffrey.johnston@qub.ac.uk>
2025-07-16 16:19:20 +0100

    initial layout and content development

--- compile.sh
+++ compile.sh
@@ -3,10 +3,15 @@
 # Exit immediately if a command fails
 set -e
 
+FILENAME=readingtypes
+
 # Compile the LaTeX file to PDF using XeLaTeX
-xelatex readingtypes.tex
+xelatex $FILENAME.tex
 
 # Convert the PDF to PNG using ImageMagick
-convert -density 300 readingtypes.pdf -background white -alpha remove -quality 100 readingtypes.png
+convert -density 300 $FILENAME.pdf \
+        -resize 1200x627^ -gravity center -extent 1200x627 \
+        -background white -alpha remove -quality 100 \
+        $FILENAME.png
 
-echo "✅ Conversion complete: readingtypes.png"
+echo "✅ Conversion complete: $FILENAME.png"
